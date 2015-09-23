344c6f370ab0d62412d10a08bb0855c761761f05
Nguyen Anh Tu <anhtu2012@gmail.com>
2015-09-23 15:47:56 +0700

    tạo khung giao diện

--- PaintJava/src/SwingJFrame.java
+++ PaintJava/src/SwingJFrame.java
@@ -1,7 +1,13 @@
-import javax.swing.JFrame;
 
-public class SwingJFrame {
- public static void main (String[] args) {
 
- }
-}
+	import javax.swing.JFrame;
+
+	public class SwingJFrame {
+	 public static void main (String[] args) {
+	JFrame giaodien = new JFrame("Paint");
+	giaodien.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	giaodien.setVisible(true);
+	giaodien.setSize(1080, 720);
+	 }
+	}
+
